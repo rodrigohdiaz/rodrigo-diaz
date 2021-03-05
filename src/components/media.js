39241ba83b66bc97c2a2570a77afad2c3993982c
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-03-05 15:46:53 +1300

    collapsable menu working

--- src/components/media.js
+++ src/components/media.js
@@ -1,5 +1,5 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import "../styles/media.scss"
 import "../styles/global.scss"
 import { Container } from "react-bootstrap";
@@ -10,25 +10,25 @@ import linkedin from "../images/linkedin.png"
 import insta from "../images/instagram.png"
 import mail from "../images/mail.png"
 
-// const ListLink = props => (
-//     <li style={{className:"nav"}}>
-//       <Link to={props.to}>{props.children}</Link>
-//     </li>
-//   )
+const ListLink = props => (
+    <li style={{className:"nav"}}>
+      <Link to={props.to}>{props.children}</Link>
+    </li>
+  )
 
 
 export default function Layout ({ children }) {
     return (
         <Container fluid-content-md-center>
                 <div className="icons">
-                  <a href="https://github.com/rodrigohdiaz" target="_blank"><img src={git} className="icon"></img></a>
-                  <a href="https://codepen.io/rodrigohdiaz" target="_blank"><img src={code} className="icon"></img></a>
-                  <a href="https://twitter.com/rodrigohdiaz" target="_blank"><img src={twit} className="icon"></img></a>
-                  <a href="https://www.linkedin.com/in/rodrigohdiaz/" target="_blank"><img src={linkedin} className="icon"></img></a>
-                  <a href="https://www.instagram.com/rodrigohdiaz/" target="_blank"><img src={insta} className="icon"></img></a>
-                  <a href="mailto: rodrigohdiaz@gmail.com" target="_blank"><img src={mail} className="icon"></img></a>
+                  <a href="https://github.com/rodrigohdiaz" target="_blank"><img src={git} className="icon" alt="git"></img></a>
+                  <a href="https://codepen.io/rodrigohdiaz" target="_blank"><img src={code} className="icon" alt="code"></img></a>
+                  <a href="https://twitter.com/rodrigohdiaz" target="_blank"><img src={twit} className="icon" alt="twitter"></img></a>
+                  <a href="https://www.linkedin.com/in/rodrigohdiaz/" target="_blank"><img src={linkedin} className="icon" alt="linkedin"></img></a>
+                  <a href="https://www.instagram.com/rodrigohdiaz/" target="_blank"><img src={insta} className="icon" alt="insta"></img></a>
+                  <a href="mailto: rodrigohdiaz@gmail.com" target="_blank"><img src={mail} className="icon" alt="mail"></img></a>
                 </div>
-                {children}
         </Container>
+        
     )
 }

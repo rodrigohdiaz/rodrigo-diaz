915f46a23735d0d0e46ae3625fa06000a74ab019
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-03-22 19:56:39 +1300

    added gallery page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,6 +7,7 @@ exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/rodrigodiaz/a-web/bootcamp/rodrigo-diaz/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/rodrigodiaz/a-web/bootcamp/rodrigo-diaz/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/rodrigodiaz/a-web/bootcamp/rodrigo-diaz/src/pages/about.js")),
+  "component---src-pages-gallery-js": preferDefault(require("/Users/rodrigodiaz/a-web/bootcamp/rodrigo-diaz/src/pages/gallery.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/rodrigodiaz/a-web/bootcamp/rodrigo-diaz/src/pages/index.js"))
 }
 
